39fafb5af3b6eef0e680487a1305c638af64f505
noahyoda <noahyoda1@gmail.com>
2021-08-18 18:18:00 -0600

    finished remove student method

--- room.py
+++ room.py
@@ -5,7 +5,7 @@ class student:
 
 
 def addStudent(room):
-    #room is the list of students we passed in we passed in
+    # room is the list of students we passed in we passed in
     print("What is the students name: ")
     stuName = input()
     print("What is the students grade level (numarical): ")
@@ -13,5 +13,20 @@ def addStudent(room):
     newStu = student(stuName, stuGrade)
     room.append(newStu)
 
+
 def removeStudent(room):
-    room.remove(name)
+    print("What is the students name you want to remove: ")
+    stuName = input()
+    oldStudent = contains(room, stuName)
+    if oldStudent != False:
+        room.remove(oldStudent)
+        print("Student was removed!\n")
+    else:
+        print("Error, student does not exist yet")
+
+
+def contains(list, name):
+    for student in list:
+        if student.name == name:
+            return student
+    return False

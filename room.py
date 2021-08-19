629e635767888aa9a8a9fbd080875f6ae5484061
noahyoda <noahyoda1@gmail.com>
2021-08-18 18:08:46 -0600

    finished add student function

--- room.py
+++ room.py
@@ -1,5 +1,17 @@
-def addStudent(room, name):
-    room.append(name)
+class student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
 
-def removeStudent(room, name):
+
+def addStudent(room):
+    #room is the list of students we passed in we passed in
+    print("What is the students name: ")
+    stuName = input()
+    print("What is the students grade level (numarical): ")
+    stuGrade = input()
+    newStu = student(stuName, stuGrade)
+    room.append(newStu)
+
+def removeStudent(room):
     room.remove(name)

5c6650d914f539a42fde6797894b7f19fb25b44a
Noah <67946197+noahyoda@users.noreply.github.com>
2021-08-25 15:47:32 -0600

    updated references and choices to export students

--- enroll.py
+++ enroll.py
@@ -1,19 +1,23 @@
 import room
+import exportStudent
 
 print("Hello and welcome to my python enrollment script")
 classroom = []
 
 while True:
-    print("What would you like to do?\n1) Add a student\n2) Remove a student\n3) view class\n4) exit program")
+    print(
+        "What would you like to do?\n1) Add a student\n2) Remove a student\n3) Export to file\n4) exit program"
+    )
     choice = input()
-    if choice == "1":
+    if choice == 1:
         room.addStudent(classroom)
-    elif choice == "2":
+    elif choice == 2:
         room.removeStudent(classroom)
-    elif choice == "3":
-        room.listStudents(classroom)
-    elif choice == "4":
+    elif choice == 3:
+        print("what file do you want to export to:")
+        file = input()
+        exportStudent.export(file, classroom)
+    elif choice == 4:
         break
     else:
         print("error: please select a real number listed in choices above!")
-#Test
